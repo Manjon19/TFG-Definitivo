a509233db6337d187537f3619e4ce41beae14ecc
Manjon19 <64155507+Manjon19@users.noreply.github.com>
2023-06-11 17:19:17 +0200

    Delete app directory

--- app/views/plantilla/pie.php
+++
@@ -1,8 +0,0 @@
-<footer class="d-flex justify-content-around w-100 position-relative bottom-0">
-  <span class="h6">Proyecto Autoescuela</span>
-  <span class="h6"> David Manjón Pérez&copy;</span>
-</footer>
-
-</body>
-
-</html>
